26e12c6cfb4086e249864a5221ef92386739e7b5
SignalBoost <cadomos@gmail.com>
2026-04-14 21:10:43 -0500

    Delete api/server.js

--- api/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-const feedRoutes = require("./api/feedServer"); // adjust path if needed
-app.use("/api", feedRoutes);
-
-app.listen(3000, () => console.log("Server running on port 3000"));
