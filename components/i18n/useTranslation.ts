93d1a09e41f25cd1017447ea5ae7fd5e605e6aba
SignalBoost <cadomos@gmail.com>
2026-05-30 00:47:54 -0600

    Update useTranslation.ts

--- components/i18n/useTranslation.ts
+++ components/i18n/useTranslation.ts
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 import en from '../../locales/en.json';
 import es from '../../locales/es.json';
+import pt from '../../locales/pt.json';
+import pl from '../../locales/pl.json';
+import ru from '../../locales/ru.json';
 
-const translations = { en, es };
+const translations: Record<string, any> = { en, es, pt, pl, ru };
 
 export default function useTranslation() {
-  const [lang, setLang] = useState('en');
+  const { locale, asPath, push } = useRouter();
+  
+  const lang = locale || 'en';
+  const tData = translations[lang] || translations['en'];
 
-  function t(key: string) {
-    return translations[lang][key] || key;
-  }
+  const t = (keyString: string): string => {
+    return keyString.split('.').reduce((obj, key) => obj?.[key], tData) || keyString;
+  };
+
+  const setLang = (newLocale: string) => {
+    push(asPath, asPath, { locale: newLocale });
+  };
 
   return { t, lang, setLang };
 }
